23ffe0975b251a92b3820366a6baab9d340ed224
john8chop <john8chop@gmail.com>
2019-05-27 16:23:46 -0400

    changed names

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -43,8 +43,8 @@ QString BitcoinUnits::longName(int unit)
     switch(unit)
     {
     case BTC: return QString("TOON");
-    case mBTC: return QString("lites");
-    case uBTC: return QString("photons");
+    case mBTC: return QString("toons");
+    case uBTC: return QString("draws");
     case SAT: return QString("liteoshi");
     default: return QString("???");
     }
